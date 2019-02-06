a0fed629ef6bd84db2f25abc65a1f787013df015
John Keely <john.t.keely@gmail.com>
2019-02-06 00:31:58 +0000

    Add files via upload

---
+++ Pythonfile1.py
@@ -0,0 +1,6 @@
+# Test file for working with Jenkins and Github
+print ("---------------------------------------")
+print ("This is the first python script to run")
+print ("This file is called Pythonfile1.py")
+print ("File Pythonfile2.py will run next")
+print ("---------------------------------------")
---
+++ Pythonfile2.py
@@ -0,0 +1,7 @@
+# Test file for working with Jenkins and Github
+print ("---------------------------------------")
+print ()
+print ("This is the second python script to run")
+print ("This file is called Pythonfile2.py")
+print ()
+print ("---------------------------------------")
